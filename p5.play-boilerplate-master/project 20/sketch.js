d81931bcf4caab5ee0018c4a21a3ea3c495c62cb
chark58 <79467667+chark58@users.noreply.github.com>
2021-03-16 21:22:32 +0530

    Add files via upload

--- p5.play-boilerplate-master/project 20/sketch.js
+++ p5.play-boilerplate-master/project 20/sketch.js
@@ -12,7 +12,7 @@ function setup() {
    weight=random(400,1500);
    
    car=createSprite(50,200,50,50);
-
+  
    wall=createSprite(1500,200,60,height/2);
    wall.shapeColour=(80,80,80);
 
@@ -22,6 +22,21 @@ function draw() {
   background(255,255,255);  
   car.velocity.x =world.mouse.x; 
 
-}
+  if(wall.x car.x < (car.width+wall.width)/2)
+  {
+  car.velocity.x= speed;
+  var deformation=0.5 * weight * speed * speed/22509;
+  if(deformation>180);
+  {
+    car.shapeColour =(250,0,0);
+  }
+  if(deformation<180  &&  deformation>100)
+  {
+    car.shapeColour=(230,230,0);
+  }
+  if(deformation>100)
+  shapeColour=(0,255,0);{
+    car.
+  
 drawSprites();
 }
